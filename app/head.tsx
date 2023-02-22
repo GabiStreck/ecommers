77407943660e32347747c6b41489ca36ee69754a
gabriel streck <gabriel@apperto.co>
2023-02-22 16:25:59 -0300

    initial commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ecommerce</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Ecommerce" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
